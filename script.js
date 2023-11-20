c510982092fb16faf9d291c08dc73a9ce271a4ae
DEEPAK SINGH JETHI <134581381+deepak-singh-jethi@users.noreply.github.com>
2023-11-20 19:55:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let para = document.getElementById("counter");
 
 count = 0;
-
 function increment() {
-  
-  para.innerText = count;
   alert(count);
-	count++;
+count++;
+  para.innerText = count;
+
+	
 }
