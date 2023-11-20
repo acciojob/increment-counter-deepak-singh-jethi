6ade2ce25c37890561c2623050cfd67a3eb45b0e
DEEPAK SINGH JETHI <134581381+deepak-singh-jethi@users.noreply.github.com>
2023-11-20 19:33:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+let para = document.getElementById("counter");
+let btn = document.getElementById("incrementBtn");
+count = 0;
+btn.addEventListener("click", (e) => {
+  count++;
+  para.innerText = count;
+});
+
+btn.addEventListner("on")
